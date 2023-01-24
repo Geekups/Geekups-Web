0a7381856a0ed5e8d9c4d523339eaf2ef8a463f2
Mohammad Javad Tabari <mohammadJavadTabari1024@outlook.com>
2023-01-24 08:56:30 -0800

    base1

---
+++ DayanaWeb/Shared/EntityFramework/Common/BaseEntity.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DayanaWeb.Shared.EntityFramework.Common;
+
+//defined for reflection
+public interface IBaseEntity
+{
+}
+public abstract class BaseEntity<T> : IBaseEntity
+{
+    public T Id { get; set; }
+
+    // last changes datetime
+    // map this shiit in your every create and update
+    public DateTime ModifiedDate { get; set; } = DateTime.Now;
+}
+
+// this is defualt for id 
+//you see default id type is long
+// but if need to have diffrent type
+// just depend yor entity directly to ----> BaseEntity<T>
+public abstract class BaseEntity : BaseEntity<long>
+{
+}
---
+++ DayanaWeb/Shared/EntityFramework/Entities/Blog/Post.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DayanaWeb.Shared.EntityFramework.Entities.Blog;
+public class Post
+{
+    public string Name { get; set; }
+}
+
