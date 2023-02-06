b9681329160ec499ede6506a4fca535ba7c06b93
Mohammad Javad Tabari <mohammadJavadTabari1024@outlook.com>
2023-02-06 11:02:16 -0800

    add mudblazor base

--- DayanaWeb/Client/Program.cs
+++ DayanaWeb/Client/Program.cs
@@ -2,10 +2,13 @@ using DayanaWeb.Client;
 using DayanaWeb.Shared.BaseServices;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using MudBlazor.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddMudServices();
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddScoped<IHttpService, HttpService>();
