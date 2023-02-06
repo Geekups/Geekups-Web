b2fa879ae16cbbef0d76ff88f426b97ad1f6d12f
Mohammad Javad Tabari <mohammadJavadTabari1024@outlook.com>
2023-02-06 04:44:19 -0800

    add category

--- DayanaWeb/Client/Pages/Admin/Blog/AddPost.razor.cs
+++ DayanaWeb/Client/Pages/Admin/Blog/AddPost.razor.cs
@@ -1,15 +1,14 @@
 ﻿using DayanaWeb.Shared.EntityFramework.DTO.Blog;
 using DayanaWeb.Shared.Infrastructure.Routes;
 
-namespace DayanaWeb.Client.Pages.Admin.Blog
-{
-    public partial class AddPost
-    {
-        PostDto postDto = new();
+namespace DayanaWeb.Client.Pages.Admin.Blog;
 
-        public async Task AddNewPost()
-        {
-            await _httpService.PostValue(Routes.Post + "add-post", postDto);
-        }
+public partial class AddPost
+{
+    PostDto model = new();
+
+    public async Task Add()
+    {
+        await _httpService.PostValue(Routes.Post + "add-post", model);
     }
 }
