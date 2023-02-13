aaf7766e4d19cfb3003c47706b573398e6a528b3
Mohammad Javad Tabari <mohammadJavadTabari1024@outlook.com>
2023-02-13 00:34:14 -0800

    pcc

---
+++ DayanaWeb/Client/Pages/Admin/Blog/Category/PostCategory.razor.cs
@@ -0,0 +1,13 @@
+﻿using DayanaWeb.Shared.EntityFramework.DTO.Blog;
+using DayanaWeb.Shared.Infrastructure.Routes;
+
+namespace DayanaWeb.Client.Pages.Admin.Blog.Category;
+
+public partial class PostCategory
+{
+    List<PostCategoryDto> model = new();
+    protected override async Task OnInitializedAsync()
+    {
+        model = await _httpService.GetValueList<PostCategoryDto>(Routes.PostCategory + "get-post-category-list-by-filter");
+    }
+}
