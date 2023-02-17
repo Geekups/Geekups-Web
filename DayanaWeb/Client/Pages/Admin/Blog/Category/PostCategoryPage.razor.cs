7c06b9f7d48600dc8222d0450335b8dcfb68ffcc
Mohammad Javad Tabari <mohammadJavadTabari1024@outlook.com>
2023-02-17 10:26:05 -0800

    break1

--- DayanaWeb/Client/Pages/Admin/Blog/Category/PostCategoryPage.razor.cs
+++ DayanaWeb/Client/Pages/Admin/Blog/Category/PostCategoryPage.razor.cs
@@ -2,30 +2,37 @@
 using DayanaWeb.Shared.EntityFramework.DTO.Blog;
 using DayanaWeb.Shared.EntityFramework.Entities.Blog;
 using DayanaWeb.Shared.Infrastructure.Routes;
+using MudBlazor;
+using static MudBlazor.CategoryTypes;
 
 namespace DayanaWeb.Client.Pages.Admin.Blog.Category;
 
 public partial class PostCategoryPage
 {
+    MudTable<PostCategory> _table = new();
+    private IEnumerable<PostCategory> _elements = new List<PostCategory>();
+    int _totalItems = 0;
+
     int _selectedPage = 1;
-    List<PostCategory> model = new();
     PaginatedList<PostCategory> paginationData = new();
 
     protected override async Task OnInitializedAsync()
     {
-        var paginatedList = await GetPaginatedListAsync(); 
-        model = paginatedList.Data;
+        var paginatedList = await GetPaginatedListAsync(_selectedPage);
         paginationData = paginatedList;
+        _elements = paginatedList.Data;
+        _totalItems = paginatedList.TotalCount;
     }
 
-    private async Task<PaginatedList<PostCategory>> GetPaginatedListAsync()
+    private async Task<PaginatedList<PostCategory>> GetPaginatedListAsync(int selectedPage)
     {
-        DefaultPaginationFilter paginationFilter = new(_selectedPage, 10);
+        DefaultPaginationFilter paginationFilter = new(selectedPage, 10);
         return await _httpService.GetPagedValue<PostCategory>(Routes.PostCategory + "get-post-category-list-by-filter", paginationFilter);
     }
 
-    private async Task ChangePageAsync(EventArgs eventArgs)
+    private async void PageChanged(int i)
     {
-        eventArgs
+        _elements = (await GetPaginatedListAsync(i)).Data;
+        _table.NavigateTo(i);
     }
 }
