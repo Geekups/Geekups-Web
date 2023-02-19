2ec28224e3703f15b0387c636eecbc121e88d542
Mohammad Javad Tabari <mohammadJavadTabari1024@outlook.com>
2023-02-19 01:01:38 -0800

    edit inline

--- DayanaWeb/Client/Pages/Admin/Blog/Category/PostCategoryPage.razor.cs
+++ DayanaWeb/Client/Pages/Admin/Blog/Category/PostCategoryPage.razor.cs
@@ -11,12 +11,18 @@ public partial class PostCategoryPage
 {
     private IEnumerable<PostCategory> pagedData;
     private MudTable<PostCategory> table;
-
+    private List<string> editEvents = new();
+    private bool dense = false;
+    private bool hover = true;
+    private bool readOnly = false;
+    private bool canCancelEdit = false;
+    private bool blockSwitch = false;
+    private string searchString = "";
+    private PostCategory selectedItem = null;
     private int totalItems;
-    private string searchString = null;
 
     /// <summary>
-    /// Here we simulate getting the paged, filtered and ordered data from the server
+    /// getting the paged, filtered and ordered data from the server
     /// </summary>
     private async Task<TableData<PostCategory>> ServerReload(TableState state)
     {
@@ -26,6 +32,10 @@ public partial class PostCategoryPage
         return new TableData<PostCategory>() { TotalItems = paginatedData.TotalCount, Items = pagedData };
     }
 
+    private async Task OnCommitEdit()
+    {
+        await _httpService.PutValue(Routes.PostCategory + "", selectedItem);
+    }
     private void OnSearch(string text)
     {
         searchString = text;
