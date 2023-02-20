8cfd2538e5b6aa3017e587580ae15efaf4999864
Mohammad Javad Tabari <mohammadJavadTabari1024@outlook.com>
2023-02-20 00:29:37 -0800

    delete act

--- DayanaWeb/Client/Pages/Admin/Blog/Category/PostCategoryPage.razor.cs
+++ DayanaWeb/Client/Pages/Admin/Blog/Category/PostCategoryPage.razor.cs
@@ -11,15 +11,9 @@ public partial class PostCategoryPage
 {
     private IEnumerable<PostCategory> pagedData;
     private MudTable<PostCategory> table;
-    private List<string> editEvents = new();
-    private bool dense = false;
-    private bool hover = true;
-    private bool readOnly = false;
-    private bool canCancelEdit = false;
-    private bool blockSwitch = false;
     private string searchString = "";
     private PostCategory selectedItem = null;
-    private int totalItems;
+    
 
     /// <summary>
     /// getting the paged, filtered and ordered data from the server
@@ -35,6 +29,10 @@ public partial class PostCategoryPage
     private async Task OnCommitEdit()
     {
         await _httpService.PutValue(Routes.PostCategory + "", selectedItem);
+    }
+    private async Task OnDelete(long id)
+    {
+        await _httpService.DeleteValue<PostCategory>(Routes.PostCategory + $"delete-post-category/{id}");
     }
     private void OnSearch(string text)
     {
